1d6fd549ad7db6c8ccf1ebd3699c1b5c4e56ae67
Rostislav71 <rostik71programist@gmail.com>
2023-03-03 12:27:40 +0200

    visualization of pictures

--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -1,13 +1,49 @@
 import React from "react";
 
 export const Collection = ({name , images}) => {
+    const [img , setImg] = React.useState(-1);
+    const [imgOverSize , setImgOverSize] = React.useState(false);
+
+    const arrImg = [images[1] , images[2] , images[3]];
+
+    const body = document.querySelector('body').classList;
+
+    function fullSizeImg(index){
+        if(img === index){
+            setImg(-1);
+            body.remove("lock");
+            return
+        }
+        setImg(index);
+        body.add("lock");
+    }
+
+    function fullSizeBigImg(){
+        if(!imgOverSize){
+            body.add("lock");
+        }
+        else{
+            body.remove("lock");
+        }
+        setImgOverSize(!imgOverSize);
+    }
     return (
         <div className="collection">
-            <img src={images[0]} className="img-big" alt="" />
+            <div 
+                onClick={() => fullSizeBigImg()} 
+                className={imgOverSize ? "img-big-body active" : "img-big-body"}>
+                <img src={images[0]} className="img-big" alt="" />
+            </div>
             <div className="collection-bottom">
-                <img src={images[1]} className="img-mini" alt="" />
-                <img src={images[2]} className="img-mini" alt="" />
-                <img src={images[3]} className="img-mini" alt="" />
+                {arrImg.map((index) => {
+                    return(
+                        <div 
+                            key={index} onClick={()=> fullSizeImg(index)} 
+                            className={img === index ? "img-body active" : "img-body"}>
+                            <img src={index} className="img-mini" alt="" />
+                        </div>
+                    )
+                })}
             </div>
             <p>{name}</p>
         </div>
